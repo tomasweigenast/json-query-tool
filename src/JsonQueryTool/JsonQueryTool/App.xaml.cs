fd1519a2e291a1b4c49bb7a7741dddfb24d466a4
Tomás Weigenast <tomaswegenast@gmail.com>
2022-11-09 11:43:49 -0300

    initial commit

---
+++ src/JsonQueryTool/JsonQueryTool/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JsonQueryTool;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ src/JsonQueryTool/JsonQueryTool/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace JsonQueryTool;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
+
