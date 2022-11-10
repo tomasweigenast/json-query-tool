96f51f87ab46436a85a5732a7c6ddcc49f89419f
Tomás Weigenast <tomaswegenast@gmail.com>
2022-11-10 17:24:00 -0300

    add where filter

--- lib/logic/query_parser.dart
+++ lib/logic/query_parser.dart
@@ -1,7 +1,14 @@
+const _kKeywords = ["SELECT", "WHERE", "LIMIT", "FROM", "ORDER", "BY"];
+const _kFilterKeywords = ["AND", "OR"];
+
 class QueryParser {
   final String query;
   
   late final List<String> tokens;
+
+  final List<String> select = [];
+  final List<WhereFilter> filters = [];
+  final List<String> ordering = [];
 
   QueryParser(this.query) {
     _parse();
@@ -9,11 +16,51 @@ class QueryParser {
 
   void _parse() {
     _tokenize();
-  
+
+    var tokens = List<String>.from(this.tokens);
+    while(true) {
+      try {
+        var token = tokens.removeLast();
+
+        // get SELECT tokens
+        if(token == "SELECT") {
+          while(true) {
+            token = tokens.removeLast();
+            if(_kKeywords.contains(token)) {
+              break;
+            }
+
+            select.add(token);
+          }
+        }
+
+        // get filters
+        if(token == "WHERE") {
+          while(true) {
+            var token = tokens.removeLast();
+            if(_kFilterKeywords.contains(token)) {
+              continue;
+            }
+
+            if(_kKeywords.contains(token)) {
+              break;
+            }
+
+            String property = token;
+            String operation = tokens.removeLast();
+            String value = tokens.removeLast();
+
+            filters.add(WhereFilter(property, operation, value));
+          }
+        }
+      } catch(_) {
+        break;
+      }
+    }
   }
 
   void _tokenize() {
-    tokens = [];
+    var tokens = <String>[];
     var tempTokens = query.split(RegExp(r"(,| )"));
     for(var tmp in tempTokens) {
       tmp = tmp.trim();
@@ -24,5 +71,30 @@ class QueryParser {
 
       tokens.add(tmp);
     }
+
+    this.tokens = tokens.reversed.toList();
+  }
+}
+
+class WhereFilter {
+  final String property;
+  final String operation;
+  final String value;
+
+  WhereFilter(this.property, this.operation, this.value);
+
+  @override
+  String toString() => "$property $operation $value";
+
+  @override
+  int get hashCode => Object.hash(property, operation, value);
+  
+  @override
+  bool operator ==(Object other) {
+    if(other is! WhereFilter) {
+      return false;
+    }
+
+    return property == other.property && operation == other.operation && value == other.value;
   }
 }
--- test/query_parser_test.dart
+++ test/query_parser_test.dart
@@ -3,7 +3,17 @@ import 'package:json_query_tool/logic/query_parser.dart';
 
 void main() {
   test("test tokenizer", () {
-    expect(QueryParser("SELECT * FROM source WHERE level.nested >= 5 ORDER BY name LIMIT 10").tokens, equals(["SELECT", "*", "FROM", "source", "WHERE", "level.nested", ">=", "5", "ORDER", "BY", "name", "LIMIT", "10"]));
-    expect(QueryParser("SELECT id, name,level WHERE level.nested >= 5").tokens, equals(["SELECT", "id", "name", "level", "WHERE", "level.nested", ">=", "5"]));
+    expect(QueryParser("SELECT * FROM source WHERE level.nested >= 5 ORDER BY name LIMIT 10").tokens, equals(["SELECT", "*", "FROM", "source", "WHERE", "level.nested", ">=", "5", "ORDER", "BY", "name", "LIMIT", "10"].reversed));
+    expect(QueryParser("SELECT id, name,level WHERE level.nested >= 5").tokens, equals(["SELECT", "id", "name", "level", "WHERE", "level.nested", ">=", "5"].reversed));
+  });
+
+  test("test parse SELECT", () {
+    expect(QueryParser("SELECT * FROM source WHERE level.nested >= 5 ORDER BY name LIMIT 10").select, equals(["*"]));
+    expect(QueryParser("SELECT id, name,level WHERE level.nested >= 5").select, equals(["id", "name", "level"]));
+  });
+
+  test("test parse WHERE", () {
+    expect(QueryParser("SELECT * FROM source WHERE level.nested >= 5 ORDER BY name LIMIT 10").filters, equals([WhereFilter("level.nested", ">=", "5")]));
+    expect(QueryParser("SELECT * WHERE level.nested < 5 AND name = 'tomas'").filters, equals([WhereFilter("level.nested", "<", "5"), WhereFilter("name", "=", "'tomas'")]));
   });
 }
